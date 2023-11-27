7b481423accaea399d06dfd1c8c3daf06e340f80
Angie <liya0706s@gmail.com>
2023-11-27 13:32:59 +0800

    tab_dynamic_col with iframe

---
+++ s20231127/test.php
@@ -0,0 +1,19 @@
+<?php
+
+$data = [
+    [
+        'name' => 'amy',
+        'mobile' => '0911-111-111',
+        'price' => 100
+    ],
+    [
+        'name' => 'bob',
+        'mobile' => '0922-222-222',
+        'price' => 200
+    ]
+];
+
+$sum =0;
+foreach($data as $key => $value){
+    
+}
