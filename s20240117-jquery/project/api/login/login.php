2905965c38efa73a938fa722555d63369650f4f8
Angie <liya0706s@gmail.com>
2024-01-17 11:13:25 +0800

    老師用生活經驗洗衣服的步驟類比寫後端php的順序，驗證資料/抓資料/sql/帳號密碼是否正確/ok?error?

---
+++ s20240117-jquery/project/api/login/login.php
@@ -0,0 +1,47 @@
+<?php
+// login
+$data=$_POST
+
+// 先寫中文--
+// 來這裡要幹嘛?
+
+// 驗證資料
+// 抓資料來驗證
+// sql ...
+
+// 帳號密碼是否正確
+
+// ok ...
+// error ...
+
+// 洗衣服的步驟
+// 1.拿出全部的衣服
+// 2.分類
+switch ($variable) {
+    case '上衣':
+        # code...
+        break;
+    case '褲子':
+        # code...
+        break;
+
+    default:
+        # code...
+        break;
+}
+
+// 3.摺衣服
+    // 上衣
+    // 褲子
+
+// 4.放到櫃子
+    // 上衣
+    // 褲子
+
+// 洗衣服 
+// 1.function 拿出全部的衣服
+// 2.function 分類
+// 3.function 摺衣服
+// 4.function 放到櫃子
+
+?>
