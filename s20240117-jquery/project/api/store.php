edf06b768670996e77a3b926abbbd3406a0c6fea
Angie <liya0706s@gmail.com>
2024-01-17 10:44:44 +0800

    介紹api和views的應用

---
+++ s20240117-jquery/project/api/store.php
@@ -0,0 +1,7 @@
+<?php
+$data= $_POST;
+
+
+// sql="INSERT INTO table column value column...."
+
+?>
---
+++ s20240117-jquery/project/api/update.php
@@ -0,0 +1,6 @@
+<?php
+$data = $_POST
+
+// sql = "UPDATE table SET column WHERE condition id = 1..."
+
+?>
